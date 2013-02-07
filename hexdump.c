a4defd9d8816b47410be94205dcd101177a90786
William Ahern <william@server.local>
2013-02-06 18:08:15 -0800

    convert to stack machine

--- hexdump.c
+++ hexdump.c
@@ -10,6 +10,7 @@
 
 enum xd_errors {
 	XD_EFORMAT = XD_EBASE,
+	XD_ECOUNT,
 	XD_ELAST
 }; /* enum xd_errors */
 
@@ -60,201 +61,13 @@ static inline int getint(const unsigned char **fmt) {
 } /* getint() */
 
 
-#define I(op)  (0x0f & (op))
-#define A0(op) (0x03 & ((op) >> 4))
-#define A1(op) (0x03 & ((op) >> 6))
-#define DST(op) AO(op)
-#define SRC(op) A1(op)
-#define VAL(op) A1(op)
-
-#define M_I(M) I((M)->code[(M)->pc])
-#define M_DST(M) ((M)->r[DST(M_I(M))])
-#define M_SRC(M) ((M)->r[SRC(M_I(M))])
-#define M_VAL(M) ((M)->r[VAL(M_I(M))])
-
-enum op {
-	I_HALT,
-	I_NOOP,
-	I_PC,
-	I_LOAD,
-	I_PUSH,
-	I_POP,
-	I_LT,
-	I_NEG,
-	I_ADD,
-	I_JMP,
-	I_REWIND,
-	I_COUNT,
-	I_PUTL,
-	I_CONV,
-};
-
-
-struct machine {
-	jmp_buf trap;
-
-	int pc;
-	int64_t r[4];
-
-	int64_t stack[8];
-	int sp;
-
-	unsigned char code[4096];
-
-	struct {
-		unsigned char *top, *p, *pe;
-		_Bool eof;
-	} i;
-
-	struct {
-		unsigned char *top, *p, *pe;
-	} o;
-}; /* struct machine */
-
-
-static int exec(struct machine *M) {
-	unsigned op;
-	int64_t *r;
-
-exec:
-	op = M->code[M->pc];
-
-	switch (I(op)) {
-	case I_HALT:
-		return 0;
-	case I_NOOP:
-		break;
-	case I_PC:
-		M->r[DST(op)] = M->pc;
-
-		break;
-	case I_LOAD:
-		r = &M->r[DST(op)];
-		*r = 0;
-
-		switch (VAL(op)) {
-		case 3:
-			*r |= (0xffU & M->code[++M->pc]);
-			*r <<= 8;
-			*r |= (0xffU & M->code[++M->pc]);
-			*r <<= 8;
-			*r |= (0xffU & M->code[++M->pc]);
-			*r <<= 8;
-			*r |= (0xffU & M->code[++M->pc]);
-			*r <<= 8;
-		case 2:
-			*r |= (0xffU & M->code[++M->pc]);
-			*r <<= 8;
-			*r |= (0xffU & M->code[++M->pc]);
-			*r <<= 8;
-		case 1:
-			*r |= (0xffU & M->code[++M->pc]);
-			*r <<= 8;
-		case 0:
-			*r |= (0xffU & M->code[++M->pc]);
-		}
-
-		break;
-	case I_PUSH:
-		M->stack[M->sp++] = M->r[SRC(op)];
-
-		break;
-	case I_POP:
-		M->r[DST(op)] = M->stack[M->sp--];
-
-		break;
-	case I_LT:
-		M_DST(M) = (M_DST(M) < M_SRC(M));
-
-		break;
-	case I_NEG:
-		M_DST(M) = -M_DST(M);
-
-		break;
-	case I_ADD:
-		M_DST(M) = M_DST(M) + M_SRC(M);
-
-		break;
-	case I_JMP:
-		if (!M->r[A0(op)])
-			break;
-
-		M->pc = M->r[A1(op)];
-
-		goto exec;
-	case I_REWIND:
-		M->p = M->block;
-
-		break;
-	case I_COUNT:
-		M->r[DST(op)] = M->pe - M->p;
-
-		break;
-	case I_PUTL:
-		*M->o.p++ = M->code[++M->pc];
-
-		break;
-	} /* switch() */
-
-	++M->pc;
-
-	goto exec;
-
-	return 0;
-} /* exec() */
-
-
-static void addpc(struct machine *M, unsigned char byte) {
-	if (M->pc >= sizeof M->code)
-		_longjmp(M->trap, ENOMEM);
-	M->code[M->pc++] = byte;
-} /* addpc() */
-
-
-static void addop(struct machine *M, unsigned char op, unsigned char a0, unsigned char a1) {
-	addpc(M, (0x0f & op) | ((0x3 & a1) << 6) | ((0x3 & a0) << 4));
-} /* addop() */
-
-
-static void putl(struct machine *M, unsigned char chr) {
-	addop(M, I_PUTL, 0, 0);
-	addpc(M, chr);
-} /* putl() */
-
-
-static void load(struct machine *M, int reg, long num) {
-	long long i = llabs(num);
-
-	if (i > ((1LL << 32) - 1)) {
-		_longjmp(M->trap, ERANGE);
-	} else if (i > ((1LL << 16) - 1)) {
-		addop(M, I_LOAD, reg, 2);
-		addpc(M, 0x0f & (num >> 24));
-		addpc(M, 0x0f & (num >> 16));
-		addpc(M, 0x0f & (num >> 8));
-		addpc(M, 0x0f & (num >> 0));
-	} else if (i > ((1LL << 8) - 1)) {
-		addop(M, I_LOAD, reg, 1);
-		addpc(M, 0x0f & (num >> 8));
-		addpc(M, 0x0f & (num >> 0));
-	} else {
-		addop(M, I_LOAD, reg, 0);
-		addpc(M, 0x0f & num);
-	}
-
-	if (num < 0) {
-		addop(M, I_NEG, reg, 0);
-	}
-} /* load() */
-
-
 #define F_HASH  1
 #define F_ZERO  2
 #define F_MINUS 4
 #define F_SPACE 8
 #define F_PLUS 16
 
-static inline int getcsp(int *flags, int *width, int *prec, int *bytes, const unsigned char **fmt) {
+static inline int getcnv(int *flags, int *width, int *prec, int *bytes, const unsigned char **fmt) {
 	int ch;
 
 	*flags = 0;
@@ -297,7 +110,7 @@ width:
 		break;
 	case 's';
 		if (*prec == -1)
-			return -1;
+			return 0;
 		*bytes = *prec;
 		break;
 	case '_':
@@ -314,7 +127,7 @@ width:
 				ch = ('_' & ('x' << 8));
 				break;
 			default:
-				return -1;
+				return 0;
 			}
 			*bytes = 0;
 			break;
@@ -330,7 +143,7 @@ width:
 				ch = ('_' & ('X' << 8));
 				break;
 			default:
-				return -1;
+				return 0;
 			}
 			*bytes = 0;
 			break;
@@ -347,7 +160,7 @@ width:
 			*bytes = 1;
 			break;
 		default:
-			return -1;
+			return 0;
 		}
 
 		break;
@@ -356,42 +169,358 @@ width:
 	++*fmt;
 
 	return ch;
-} /* getcsp() */
+} /* getcnv() */
 
 
-static inline size_t getfmt(unsigned char *code, size_t lim, const unsigned char **fmt) {
+enum vm_opcode {
+	OP_HALT,  /* 0/0 */
+	OP_NOOP,  /* 0/0 */
+	OP_TRAP,  /* 0/0 */
+	OP_PC,    /* 0/1 | push program counter */
+	OP_TRUE,  /* 0/1 | push true */
+	OP_I8,    /* 0/1 | load 8-bit unsigned int from code */
+	OP_I16,   /* 0/1 | load 16-bit unsigned int from code */
+	OP_I32,   /* 0/1 | load 32-bit unsigned int from code */
+	OP_NEG,   /* 1/1 | arithmetic negative */
+	OP_SUB,   /* 2/1 | S(-2) - S(-1) */
+	OP_ADD,   /* 2/1 | S(-2) + S(-1) */
+	OP_NOT,   /* 1/1 | logical not */
+	OP_DUP,   /* 1/2 | dup top of stack */
+	OP_SWAP,  /* 2/2 | swap values at top of stack */
+	OP_READ,  /* 1/1 | read bytes from input buffer */
+	OP_COUNT, /* 0/1 | count of bytes in input buffer */
+	OP_COPY,  /* 0/0 | copy char from input to output buffer */
+	OP_CONV,  /* 5/0 | write conversion to output buffer */
+	OP_TRIM,  /* 0/0 | trim trailing white space from output buffer */
+	OP_JMP,   /* 2/0 | conditional jump to address */
+}; /* enum vm_opcode */
+
+
+struct vm_state {
+	jmp_buf trap;
+
+	int blksize;
+
+	int64_t stack[8];
+	int sp;
+
+	unsigned char code[4096];
+	int pc;
+
+	struct {
+		unsigned char *base, *p, *pe;
+		_Bool eof;
+	} i;
+
+	struct {
+		unsigned char *base, *p, *pe;
+	} o;
+}; /* struct vm_state */
+
+
+static void vm_throw(struct vm_state *M, int error) {
+	_longjmp(M->trap, error);
+} /* vm_throw() */
+
+
+static void vm_push(struct vm_state *M, int64_t v) {
+	M->stack[M->sp++] = v;
+} /* vm_push() */
+
+
+static int64_t vm_pop(struct vm_state *M) {
+	return M->stack[--M->sp];
+} /* vm_pop() */
+
+
+static int64_t vm_peek(struct vm_state *M, int i) {
+	return (i < 0)? M->stack[M->sp + p] : M->stack[i];
+} /* vm_peek() */
+
+
+static int vm_exec(struct vm_state *M) {
+	enum vm_opcode op;
+
+exec:
+	op = M->code[M->pc];
+
+	switch (op) {
+	case OP_HALT:
+		return 0;
+	case OP_NOOP:
+		break;
+	case OP_TRAP:
+		vm_throw(M, EFAULT);
+
+		break;
+	case OP_PC:
+		vm_push(M, M->pc);
+
+		break;
+	case OP_TRUE:
+		vm_push(M, 1);
+
+		break;
+	case OP_I8:
+		vm_push(M, M->code[++M->pc]);
+
+		break;
+	case OP_I16:
+		vm_push(M, (M->code[++M->pc] << 8) | M->code[++M->pc]);
+
+		break;
+	case OP_I32: {
+		int64_t v = (M->code[++M->pc] << 24) | (M->code[++M->pc] << 16)
+		          | (M->code[++M->pc] << 8)  | (M->code[++M->pc] << 0)
+
+		vm_push(M, v);
+
+		break;
+	}
+	case OP_NEG:
+		vm_push(M, -vm_pop(M));
+
+		break;
+	case OP_SUB: {
+		int64_t b = vm_pop(M);
+		int64_t a = vm_pop(M);
+
+		vm_push(M, a - b);
+
+		break;
+	}
+	case OP_ADD: {
+		int64_t b = vm_pop(M);
+		int64_t a = vm_pop(M);
+
+		vm_push(M, a + b);
+
+		break;
+	}
+	case OP_NOT:
+		vm_push(M, !vm_pop(M));
+
+		break;
+	case OP_DUP: {
+		int64_t v = vm_pop(M);
+
+		vm_push(M, v);
+		vm_push(M, v);
+
+		break;
+	}
+	case OP_SWAP: {
+		int64_t x = vm_pop(M);
+		int64_t y = vm_pop(M);
+
+		vm_push(M, x);
+		vm_push(M, y);
+
+		break;
+	}
+	case OP_READ: {
+		int64_t i, n, v;
+
+		n = vm_pop(M);
+		v = 0;
+
+		for (i = 0; i < n && M->i.p < M->i.pe; i++) {
+			v <<= 8;
+			v |= *M->i.p++;
+		}
+
+		break;
+	}
+	case OP_COUNT:
+		vm_push(M, M->i.pe - M->i.p);
+
+		break;
+	case OP_COPY: {
+		unsigned char ch = M->code[++M->pc];
+
+		if (M->o.p < M->o.pe)
+			*M->o.p++ = ch;
+
+		break;
+	}
+	case OP_CONV: {
+		int spec = vm_pop(M);
+		int width = vm_pop(M);
+		int prec = vm_pop(M);
+		int flags = vm_pop(M);
+		int64_t word = vm_pop(M);
+
+		break;
+	}
+	case OP_TRIM:
+		while (M->o.p > M->o.base && (M->o.p[-1] == ' ' || M->o.p[-1] == '\t'))
+			--M->o.p;
+
+		break;
+	case OP_JMP: {
+		int64_t pc = vm_pop(M);
+
+		if (vm_pop(M)) {
+			M->pc = pc;
+			goto exec;
+		}
+
+		break;
+	}
+	} /* switch() */
+
+	++M->pc;
+
+	goto exec;
+
+	return 0;
+} /* vm_exec() */
+
+
+static void emit_op(struct vm_state *M, unsigned char code) {
+	if (M->pc >= sizeof M->code)
+		vm_throw(M, ENOMEM);
+	M->code[M->pc++] = code;
+} /* emit_op() */
+
+
+static void emit_int(struct vm_state *M, int64_t i) {
+	_Bool isneg;
+
+	if ((isneg = (i < 0)))
+		i * -1;
+
+	if (i > ((1LL << 32) - 1)) {
+		vm_throw(M, ERANGE);
+	} else if (i > ((1LL << 16) - 1)) {
+		emit_op(M, OP_I32);
+		emit_op(M, 0xff & (i >> 24));
+		emit_op(M, 0xff & (i >> 16));
+		emit_op(M, 0xff & (i >> 8));
+		emit_op(M, 0xff & (i >> 0));
+	} else if (i > ((1LL << 8) - 1)) {
+		emit_op(M, OP_I16);
+		emit_op(M, 0xff & (i >> 8));
+		emit_op(M, 0xff & (i >> 0));
+	} else {
+		emit_op(M, OP_I8);
+		emit_op(M, 0xff & i);
+	}
+
+	if (isneg) {
+		emit_op(M, OP_NEG);
+	}
+} /* emit_int() */
+
+
+static void emit_putc(struct vm_state *M, unsigned char chr) {
+	emit_op(M, OP_COPY);
+	emit_op(M, chr);
+} /* emit_putc() */
+
+
+static void emit_jmp(struct vm_state *M, int *from) {
+	*from = M->pc;
+	emit_op(M, OP_TRAP);
+	emit_op(M, OP_TRAP);
+	emit_op(M, OP_TRAP);
+	emit_op(M, OP_TRAP);
+	emit_op(M, OP_TRAP);
+	emit_op(M, OP_TRAP);
+} /* emit_jmp() */
+
+
+static void emit_link(struct vm_state *M, int from, int to) {
+	int pc = M->pc;
+
+	emit_op(M, OP_PC);
+
+	if (to < from) {
+		if (from - to > 65535)
+			vm_throw(M, ERANGE);
+
+		emit_op(M, OP_I16);
+		M->code[M->pc++] = 0xff & ((from - to) >> 8);
+		M->code[M->pc++] = 0xff & ((from - to) >> 0);
+		emit_op(M, OP_SUB);
+	} else {
+		if (to - from > 65535)
+			vm_throw(M, ERANGE);
+
+		emit_op(M, OP_I16);
+		M->code[M->pc++] = 0xff & ((to - from) >> 8);
+		M->code[M->pc++] = 0xff & ((to - from) >> 0);
+		emit_op(M, OP_ADD);
+	}
+
+	emit_op(M, OP_JMP);
+
+	M->pc = pc;
+} /* emit_link() */
+
+
+static void emit_unit(struct vm_state *M, int loop, int limit, const unsigned char **fmt) {
 	_Bool quoted = 0, escaped = 0;
-	unsigned char *cp, *pe;
-	int ch, cs, flags, width, prec, bytes;
+	int consumes = 0;
+	int L1, L2, from, ch;
 
-	cp = code;
-	pe = &code[lim];
+	loop = MAX(0, loop);
+
+	/* loop counter */
+	emit_int(M, 0);
+
+	/* top of loop */
+	L1 = M->pc;
+	emit_op(M, OP_DUP); /* dup counter */
+	emit_int(M, loop);  /* push loop count */
+	emit_op(M, OP_SWAP);
+	emit_op(M, OP_SUB); /* loop - counter */
+	emit_op(M, OP_NOT);
+	emit_jmp(M, &L2);
 
 	for (; (ch = **fmt); ++*fmt) {
 		switch (ch) {
-		case '%':
+		case '%': {
+			int fc, flags, width, prec, bytes;
+			int from;
+
 			if (escaped)
 				goto copyout;
 
 			++*fmt;
 
-			cs = getspec(&flags, &width, &prec, &bytes, fmt);
-
-			if (cs == -1)
-				goto badfmt;
-
-			if (cs == '%') {
+			if (!(fc = getcnv(&flags, &width, &prec, &bytes, fmt))) {
+				vm_throw(M, XD_EFORMAT);
+			} else if (fc == '%') {
 				ch = '%';
 				goto copyout;
 			}
 
-			load(M, cs);
-			load(M, flags);
-			load(M, width);
-			load(M, prec);
-			addop(M, I_CONV)
+			if (limit >= 0 && bytes > 0) {
+				bytes = MIN(limit - consumes, bytes);
+
+				if (!bytes) /* FIXME: define better error */
+					vm_throw(M, XD_NOBUFS);
+			}
+
+			consumes += bytes;
+
+			emit_op(M, OP_COUNT);
+			emit_op(M, OP_NOT);
+			emit_jmp(M, &from);
+
+			emit_int(M, bytes);
+			emit_op(M, OP_READ);
+			emit_int(M, flags);
+			emit_int(M, width);
+			emit_int(M, prec);
+			emit_int(M, fc);
+			emit_op(M, OP_CONV);
+
+			emit_link(M, from, M->pc);
 
 			break;
+		}
 		case ' ': case '\t':
 			if (quoted || escaped)
 				goto copyout;
@@ -453,33 +582,38 @@ static inline size_t getfmt(unsigned char *code, size_t lim, const unsigned char
 			goto copyout;
 		default:
 copyout:
-			if (pe - cp < 2)
-				goto epilog;
-
-			*cp++ = I_PUTL;
-			*cp++ = ch;
+			emit_copy(M, ch);
 
 			escaped = 0;
 		}
 	}
+
+	emit_op(M, OP_TRUE);
+	emit_jmp(M, &from);
+	emit_link(M, from, L1);
+
 epilog:
-	return cp - code;
-badfmt:
-	return 0;
-} /* getfmt() */
+	emit_link(M, L2, M->pc);
+	emit_op(M, OP_POP); /* pop loop counter */
+
+	if (consumes * loop > M->blksize)
+		M->blksize = consumes * loop;
+
+	return /* void */;
+} /* emit_unit() */
 
 
 static int parse(const unsigned char *fmt) {
-	int iterations, bytes;
+	int iterations, count;
 
 	while (skipws(&fmt, 1)) {
 		iterations = getint(&fmt);
 
 		if ('/' == skipws(&fmt, 0)) {
 			fmt++;
-			bytes = getint(&fmt);
+			count = getint(&fmt);
 		} else {
-			bytes = -1;
+			count = -1;
 		}
 
 		skipws(&fmt, 0);
